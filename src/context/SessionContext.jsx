0edad790927a86cdbfdfef1c993c0da55d70a705
chigirlat <lukmmanuel@gmail.com>
2024-09-11 14:12:57 +0100

    Session Bug Fixed with idle timer

--- src/context/SessionContext.jsx
+++ src/context/SessionContext.jsx
@@ -1,51 +1,24 @@
-import { createContext, useEffect, useState } from "react";
-import {clear} from 'idb-keyval'
-import {useNavigate} from 'react-router-dom'
-import { onPrompt } from "../utils/notifications/onPrompt";
+import { createContext, useEffect, useRef } from "react";
+import { clear } from "idb-keyval";
+import { useNavigate } from "react-router-dom";
+import { useIdleTimer } from "react-idle-timer";
 
 export const SessionContext = createContext();
 
 export const SessionContextProvider = ({ children }) => {
-  const [timeoutId, setTimeoutId] = useState(null);
   const navigate = useNavigate();
 
-
-  const resetInactivityTimer = () => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-      // console.log('cleared succesfully', timeoutId)
-    }
-
-    const newTimeoutId = setTimeout(() => {
-      // Clear session and redirect to login after inactivity
-      handleLogout();
-      // console.log('count down')
-    }, 300000); // 5 minutes of inactivity
-    setTimeoutId(newTimeoutId);
-  };
-
-  useEffect(() => {
-    const handleUserActivity = () => {
-      resetInactivityTimer();
-    };
-
-    window.addEventListener("mousemove",()  => {handleUserActivity();});
-    window.addEventListener("keypress", handleUserActivity);
-
-    return () => {
-      window.removeEventListener("mousemove", handleUserActivity);
-      window.removeEventListener("keypress", handleUserActivity);
-    };
-  }, [timeoutId]);
-
- 
-
-  const handleLogout = async () => {
-    localStorage.clear()
-    // onPrompt('Session Expired')
-    await clear()
+  const onIdle = () => {
+    localStorage.clear();
+    clear();
     navigate("/");
+    console.log("Triggered");
   };
+
+  useIdleTimer({
+    timeout: 15000,
+    onIdle,
+  });
 
   return (
     <SessionContext.Provider value={{}}>{children}</SessionContext.Provider>
