b526368131e47718b16d265ab13e3c8f98fe3ca4
chigirlat <lukmmanuel@gmail.com>
2024-09-11 12:21:54 +0100

    Session Bug Fixed

--- src/context/SessionContext.jsx
+++ src/context/SessionContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import {clear} from 'idb-keyval'
 import {useNavigate} from 'react-router-dom'
+import { onPrompt } from "../utils/notifications/onPrompt";
 
 export const SessionContext = createContext();
 
@@ -8,33 +9,40 @@ export const SessionContextProvider = ({ children }) => {
   const [timeoutId, setTimeoutId] = useState(null);
   const navigate = useNavigate();
 
+
+  const resetInactivityTimer = () => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+      // console.log('cleared succesfully', timeoutId)
+    }
+
+    const newTimeoutId = setTimeout(() => {
+      // Clear session and redirect to login after inactivity
+      handleLogout();
+      // console.log('count down')
+    }, 300000); // 5 minutes of inactivity
+    setTimeoutId(newTimeoutId);
+  };
+
   useEffect(() => {
     const handleUserActivity = () => {
       resetInactivityTimer();
     };
 
-    window.addEventListener("mousemove", handleUserActivity);
-    window.addEventListener("keydown", handleUserActivity);
+    window.addEventListener("mousemove",()  => {handleUserActivity();});
+    window.addEventListener("keypress", handleUserActivity);
 
     return () => {
       window.removeEventListener("mousemove", handleUserActivity);
-      window.removeEventListener("keydown", handleUserActivity);
+      window.removeEventListener("keypress", handleUserActivity);
     };
-  }, []);
+  }, [timeoutId]);
 
-  const resetInactivityTimer = () => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    const newTimeoutId = setTimeout(() => {
-      // Clear session and redirect to login after inactivity
-      handleLogout();
-    }, 300000); // 5 minutes of inactivity
-    setTimeoutId(newTimeoutId);
-  };
+ 
 
   const handleLogout = async () => {
     localStorage.clear()
+    // onPrompt('Session Expired')
     await clear()
     navigate("/");
   };
