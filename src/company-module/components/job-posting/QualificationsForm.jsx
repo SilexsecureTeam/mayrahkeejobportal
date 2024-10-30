9fb29b329fdfc4b0cb09b25e00e18577c30f54f7
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-30 01:40:11 -1100

    Update QualificationsForm.jsx

--- src/company-module/components/job-posting/QualificationsForm.jsx
+++ src/company-module/components/job-posting/QualificationsForm.jsx
@@ -11,8 +11,8 @@ function QualificationsForm({ jobUtils }) {
         currentQualification,
       ];
       jobUtils.setDetails({ ...jobUtils.details, qualification: newList });
+      setCurrentQualification('');
     }
-    setCurrentQualification('');
   };
 
   const removeQualification = (index) => {
@@ -21,47 +21,40 @@ function QualificationsForm({ jobUtils }) {
     jobUtils.setDetails({ ...jobUtils.details, qualification: newList });
   };
 
-  const qualificationList = jobUtils?.details?.qualification?.map((current, index) => (
-    <li
-      key={index}
-      className="border py-1 flex justify-between items-center gap-2 px-2 text-little text-white bg-primaryColor/80"
-    >
-      {current}
-      <IoIosCloseCircleOutline
-        onClick={() => removeQualification(index)}
-        className="text-lg cursor-pointer text-red-800"
-      />
-    </li>
-  ));
+  const qualificationList = jobUtils?.details?.qualification?.map((current, index) => {
+    return (
+      <li className="border py-[3px] flex gap-[5px] px-1 text-little text-white bg-primaryColor/80">
+        {current}
+        <IoIosCloseCircleOutline onClick={() => removeQualification(index)} className="text-lg cursor-pointer text-red-800" />
+      </li>
+    );
+  });
 
   useEffect(() => console.log(jobUtils.details.qualification), [jobUtils.details.qualification]);
 
   return (
-    <div className="flex flex-col sm:flex-row gap-4 border-b py-2">
-      <div className="flex flex-col max-w-[25%] w-full gap-2">
+    <div className="flex gap-[15%] border-b py-2 ">
+      <div className="flex flex-col max-w-[25%] w-full gap-[10px]">
         <h3 className="text-gray-700 text-sm font-semibold">Required Qualifications</h3>
-        <span className="text-little text-gray-400">
+        <span className="text-little text-gray-400 w-full md:w-auto">
           Add required qualifications for the job
         </span>
       </div>
 
-      <div className="flex flex-col gap-2 w-full sm:max-w-[70%]">
-        <div className="flex gap-2">
+      <div className="flex flex-col gap-[10px] w-full">
+        <div className="flex gap-[10px]">
           <input
             value={currentQualification}
             onChange={(e) => setCurrentQualification(e.target.value)}
             placeholder="e.g masters degree"
-            className="flex-grow p-1 outline-none border text-little"
+            className="w-[120px] p-1 outline-none border text-little"
           />
-          <button
-            onClick={addQualification}
-            className="border p-1 w-fit text-little hover:bg-primaryColor hover:text-white border-primaryColor text-primaryColor"
-          >
+          <button onClick={addQualification} className="border p-1 w-fit text-little hover:bg-primaryColor hover:text-white border-primaryColor text-primaryColor">
             Add Qualification
           </button>
         </div>
 
-        <ul className="text-[10px] text-gray-400">
+        <ul className="grid grid-cols-2 gap-2 text-[10px] text-gray-400">
           {qualificationList}
         </ul>
       </div>
