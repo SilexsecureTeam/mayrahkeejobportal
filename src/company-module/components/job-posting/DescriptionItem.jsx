0b647c15d8c4aa33a5ffaef1685e9d7883055110
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-10-30 03:02:27 -1100

    Update DescriptionItem.jsx

--- src/company-module/components/job-posting/DescriptionItem.jsx
+++ src/company-module/components/job-posting/DescriptionItem.jsx
@@ -4,22 +4,23 @@ import "react-quill/dist/quill.snow.css";
 
 function DescriptionItem({ data, jobUtils }) {
   const [content, setContent] = useState(data.placeholder);
+
   return (
-    <div className="flex gap-[15%] border-b min-h-[170px] py-2 ">
+    <div className="flex flex-col md:flex-row border-b py-2 min-h-[170px]">
       <div className="flex flex-col min-w-[25%] max-w-[25%] gap-[10px]">
         <h3 className="text-gray-700 text-sm font-semibold">{data.title}</h3>
         <span className="text-little text-gray-400">{data.desc}</span>
       </div>
 
-      <div className="flex flex-col gap-[3px] w-[60%]  text-gray-400 justify-between ">
+      <div className="flex flex-col gap-[3px] w-full text-gray-400 justify-between">
         <div className="h-[90%]">
-        <ReactQuill
-          placeholder={data.placeholder}
-          value={jobUtils.details[data.name]}
-          onChange={(text) => {
-            jobUtils.setDetails({...jobUtils.details, [data.name]: text})
-          }}
-        />
+          <ReactQuill
+            placeholder={data.placeholder}
+            value={jobUtils.details[data.name]}
+            onChange={(text) => {
+              jobUtils.setDetails({ ...jobUtils.details, [data.name]: text });
+            }}
+          />
         </div>
 
         <span className="text-[10px] text-gray-400">
