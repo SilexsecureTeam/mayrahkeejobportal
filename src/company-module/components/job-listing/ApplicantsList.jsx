0b9a64d9cbf7e4478f6ecdeff6ae8e12b6603a48
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-01 00:21:03 -1100

    Update ApplicantsList.jsx

--- src/company-module/components/job-listing/ApplicantsList.jsx
+++ src/company-module/components/job-listing/ApplicantsList.jsx
@@ -3,7 +3,7 @@ import ListingRow from "./ListingRow";
 
 function ApplicantsList({applicants}) {
     return ( 
-        <table className="min-w-full bg-white border border-gray-200">
+        <table className="min-w-full overflow-x-auto bg-white border border-gray-200">
          <thead className="border bg-white  text-gray-600 font-semibold ">
           <tr className=" text-little  divide-gray-200">
             <th className="px-4 py-1 text-center">
