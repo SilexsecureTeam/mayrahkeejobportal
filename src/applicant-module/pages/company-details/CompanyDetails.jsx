9c2625d822584d5f23dd969ee7e1326d840d289b
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-01 01:05:16 -1100

    Update CompanyDetails.jsx

--- src/applicant-module/pages/company-details/CompanyDetails.jsx
+++ src/applicant-module/pages/company-details/CompanyDetails.jsx
@@ -1,73 +1,62 @@
-import React from 'react'
-import headerImg from "../../../assets/pngs/detail-logo.png"
-import { BsShare } from 'react-icons/bs'
-import { FaRegCheckCircle } from 'react-icons/fa'
-import healthIcon from "../../../assets/pngs/perks-n-benefits/tethoscope.png"
-import vacation from "../../../assets/pngs/perks-n-benefits/vacation.png"
-import cam from "../../../assets/pngs/perks-n-benefits/cam.png"
-import arrowUp from "../../../assets/pngs/perks-n-benefits/kite-up.png"
-import coffee from "../../../assets/pngs/perks-n-benefits/coffee-cup.png"
-import trainIcon from "../../../assets/pngs/perks-n-benefits/vehicle.png"
-import unity from "../../../assets/pngs/perks-n-benefits/unity.png"
+import React from 'react';
+import headerImg from "../../../assets/pngs/detail-logo.png";
+import { BsShare } from 'react-icons/bs';
+import { FaRegCheckCircle, FaArrowLeft } from 'react-icons/fa';
+import healthIcon from "../../../assets/pngs/perks-n-benefits/tethoscope.png";
+import vacation from "../../../assets/pngs/perks-n-benefits/vacation.png";
+import cam from "../../../assets/pngs/perks-n-benefits/cam.png";
+import arrowUp from "../../../assets/pngs/perks-n-benefits/kite-up.png";
+import coffee from "../../../assets/pngs/perks-n-benefits/coffee-cup.png";
+import trainIcon from "../../../assets/pngs/perks-n-benefits/vehicle.png";
+import unity from "../../../assets/pngs/perks-n-benefits/unity.png";
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { FaArrowLeft } from "react-icons/fa";
-
-// import JobApplicationForm from './components/JobApplicationForm'
 
 const CompanyDetails = () => {
     const { state } = useLocation();
-    console.log(state)
-    const job = state?.company
+    const job = state?.company;
     const navigate = useNavigate();
-    const postedDate = new Date(job.created_at)
-    const keywordArr = job.search_keywords?.split(',')
+    const postedDate = new Date(job.created_at);
+    const keywordArr = job.search_keywords?.split(',');
 
     return (
         <div className="h-full text-[#25324b] w-full">
-            <div className=' p-6 border-b mb-8'>
+            <div className='p-6 border-b mb-8'>
                 <div className="job_header p-6">
                     <div className="p-3 bg-white border">
                         <div className="flex justify-between items-center">
                             <div className="flex">
-                                <div>
-                                    <img src={headerImg} width={80} alt="" />
-                                </div>
+                                <img src={headerImg} width={80} alt={`${job.company_name} logo`} />
                                 <div className="ml-3">
                                     <p className="text-base mb-4 font-bold">{job.company_name}</p>
                                     <p className="mb-3">· {job.location} · {job.type}</p>
-                                    {/* <p className=""><b>Address:</b> {job.address}</p> */}
                                 </div>
                             </div>
-                            <div className="flex items-center">
-                                <div className=" border-r">
-                                    <button className=" mr-5">
-                                        <BsShare />
-                                    </button>
-                                </div>
-                            </div>
+                            <button className="border-r mr-5">
+                                <BsShare />
+                            </button>
                         </div>
                     </div>
                 </div>
                 <button
                     onClick={() => navigate("/applicant/browse-companies")}
-                    className='mt-4'>
-                    <FaArrowLeft size={20} />
+                    className='mt-4 flex items-center'>
+                    <FaArrowLeft size={20} className="mr-1" /> Back to Companies
                 </button>
                 <div className="my-10">
-                    <div className="flex px-10">
-                        <div className="w-[80%] pr-4">
+                    <div className="flex flex-col md:flex-row px-10">
+                        <div className="w-full md:w-[80%] pr-4">
                             <div className="mb-6">
-                                <h4 className='font-bold mb-4'>Address:</ h4>
-                                <p> {job.address} </p>
+                                <h4 className='font-bold mb-4'>Address:</h4>
+                                <p>{job.address}</p>
                             </div>
                             <div className="mb-6">
-                                <h4 className='font-bold mb-4'>Description</ h4>
+                                <h4 className='font-bold mb-4'>Description:</h4>
                                 <p dangerouslySetInnerHTML={{ __html: job.company_profile }} />
                             </div>
                         </div>
-                        <div className="w-[20%]">
+                        <div className="w-full md:w-[20%] md:pl-4">
                             <h4 className="font-bold mb-4">More Information</h4>
-                            <div className="text-sm p-2 bg-gray-100">
+                            <div className="text-sm p-2 bg-gray-100 mb-6">
                                 <div className="flex my-4 bg-gray-300">
                                     <div className="pt-1 bg-[#56CDAD] w-[50%]"></div>
                                 </div>
@@ -83,7 +72,7 @@ const CompanyDetails = () => {
                                     <p className="font-medium">{job.rc_number}</p>
                                 </div>
                                 <div className="flex my-3 justify-between">
-                                    <p>Newtork:</p>
+                                    <p>Network:</p>
                                     <p className="font-medium">{job.network}</p>
                                 </div>
                                 <div className="flex my-3 justify-between">
@@ -98,21 +87,14 @@ const CompanyDetails = () => {
                                     <p>Sector:</p>
                                     <p className="font-medium">{job.sector}</p>
                                 </div>
-                                {/* <div className="flex my-3 justify-between">
-                                    <p>Experience</p>
-                                    <p dangerouslySetInnerHTML={{ __html: job.experience }} />
-                                    <p className="font-medium">{job.experience}</p>
-                                </div> */}
-                                <div className="fle my-3 justify-between">
+                                <div className="flex my-3 justify-between">
                                     <p>Socials:</p>
-                                    {job.social_media?.map((social) => (
-                                        <Link>
-                                            <p key={social} className="font-medium">{social}</p>
-                                        </Link>
+                                    {job.social_media?.map((social, index) => (
+                                        <Link key={index} to={social.url} className="font-medium">{social.name}</Link>
                                     ))}
                                 </div>
                                 <div className="flex my-3 justify-between">
-                                    <p>Currency</p>
+                                    <p>Currency:</p>
                                     <p className="font-medium">{job.currency}</p>
                                 </div>
                             </div>
@@ -124,103 +106,37 @@ const CompanyDetails = () => {
                                     ))}
                                 </div>
                             </div>
-                            {/* <div className="my-6 border-b pb-6">
-                                <h4 className="font-bold mb-4">Required Skills</h4>
-                                <div className="flex flex-wrap">
-                                    <button className="m-1 px-2 py-1 rounded bg-green-100 hover:bg-green-200 hover:text-green-900 text-green-700">Project Management</button>
-                                    <button className="m-1 px-2 py-1 rounded bg-green-100 hover:bg-green-200 hover:text-green-900 text-green-700">Copywriting</button>
-                                    <button className="m-1 px-2 py-1 rounded bg-green-100 hover:bg-green-200 hover:text-green-900 text-green-700">English</button>
-                                    <button className="m-1 px-2 py-1 rounded bg-green-100 hover:bg-green-200 hover:text-green-900 text-green-700">social Media Marketing</button>
-                                    <button className="m-1 px-2 py-1 rounded bg-green-100 hover:bg-green-200 hover:text-green-900 text-green-700">Cpoy Editing</button>
-                                </div>
-                            </div> */}
                         </div>
                     </div>
                 </div>
             </div>
             <div className="p-6 border-b">
                 <h4 className="font-bold text-xl">Perks & Benefits</h4>
-                <p>This job comes with several perks and benefits</p>
-                <div className="my-5 pb-4  flex flex-wrap text-sm">
-                    <div className="p-2 w-full md:w-1/4">
-                        <div className="hover:shadow p-2 h-full transition-all hover:scale-105">
-                            <div className="my-2">
-                                <img src={healthIcon} alt="" />
+                <p>This job comes with several perks and benefits:</p>
+                <div className="my-5 pb-4 flex flex-wrap text-sm">
+                    {[
+                        { img: healthIcon, title: "Full Healthcare", desc: "We believe in thriving communities and that starts with our team being happy and healthy." },
+                        { img: vacation, title: "Unlimited Vacation", desc: "We believe you should have a flexible schedule that makes space for family, wellness, and fun." },
+                        { img: cam, title: "Skill Development", desc: "We believe in always learning and leveling up our skills. Whether it's a conference or online course." },
+                        { img: arrowUp, title: "Team Summit", desc: "Every 6 months we have a full team summit where we have fun, reflect, and plan for the upcoming quarter." },
+                        { img: coffee, title: "Remote Working", desc: "We believe in thriving communities and that starts with our team being happy and healthy." },
+                        { img: trainIcon, title: "Commuter Benefits", desc: "We’re grateful for all the time and energy each team member puts into getting to work every day." },
+                        { img: unity, title: "We give back", desc: "We anonymously match any donation our employees make (up to $/€ 600) so they can support the organizations they care about most—times two." }
+                    ].map((perk, index) => (
+                        <div key={index} className="p-2 w-full md:w-1/4">
+                            <div className="hover:shadow p-2 h-full transition-all hover:scale-105">
+                                <div className="my-2">
+                                    <img src={perk.img} alt={perk.title} />
+                                </div>
+                                <h6 className="font-bold mb-3">{perk.title}</h6>
+                                <p>{perk.desc}</p>
                             </div>
-                            <h6 className="font-bold mb-3">Full Healthcare </h6>
-                            <p>We believe in thriving communities and that starts with our team being happy and healthy.</p>
                         </div>
-                    </div>
-                    <div className="p-2 w-full md:w-1/4">
-                        <div className="hover:shadow p-2 h-full transition-all hover:scale-105">
-                            <div className="my-2">
-                                <img src={vacation} alt="" />
-                            </div>
-                            <h6 className="font-bold mb-3">Unlimited Vacation </h6>
-                            <p>
-                                We believe you should have a flexible schedule that makes space for family, wellness, and fun.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="p-2 w-full md:w-1/4">
-                        <div className="hover:shadow p-2 h-full transition-all hover:scale-105">
-                            <div className="my-2">
-                                <img src={cam} alt="" />
-                            </div>
-                            <h6 className="font-bold mb-3">Skill Development</h6>
-                            <p>
-                                We believe in always learning and leveling up our skills. Whether it's a conference or online course.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="p-2 w-full md:w-1/4">
-                        <div className="hover:shadow p-2 h-full transition-all hover:scale-105">
-                            <div className="my-2">
-                                <img src={arrowUp} alt="" />
-                            </div>
-                            <h6 className="font-bold mb-3">Team Summit</h6>
-                            <p>
-                                Every 6 months we have a full team summit where we have fun, reflect, and plan for the upcoming quarter.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="p-2 w-full md:w-1/4">
-                        <div className="hover:shadow p-2 h-full transition-all hover:scale-105">
-                            <div className="my-2">
-                                <img src={coffee} alt="" />
-                            </div>
-                            <h6 className="font-bold mb-3">Remote Working</h6>
-                            <p>
-                                We believe in thriving communities and that starts with our team being happy and healthy.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="p-2 w-full md:w-1/4">
-                        <div className="hover:shadow p-2 h-full transition-all hover:scale-105">
-                            <div className="my-2">
-                                <img src={trainIcon} alt="" />
-                            </div>
-                            <h6 className="font-bold mb-3">Commuter Benefits</h6>
-                            <p>
-                                We’re grateful for all the time and energy each team member puts into getting to work every day.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="p-2 w-full md:w-1/4">
-                        <div className="hover:shadow p-2 h-full transition-all hover:scale-105">
-                            <div className="my-2">
-                                <img src={unity} alt="" />
-                            </div>
-                            <h6 className="font-bold mb-3">We give back</h6>
-                            <p>
-                                We anonymously match any donation our employees make (up to $/€ 600) so they can support the organizations they care about most—times two.
-                            </p>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default CompanyDetails
+export default CompanyDetails;
