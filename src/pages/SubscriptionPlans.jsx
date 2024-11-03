c0044497aa19e93557b2005431ae05da31922356
unknown <afolabimubarak18@gmail.com>
2024-11-02 13:27:49 -1100

    adjustment

--- src/pages/SubscriptionPlans.jsx
+++ src/pages/SubscriptionPlans.jsx
@@ -2,7 +2,7 @@ import SubscriptionCard from "../components/subscription/SubscriptionCard";
 
 function SubscriptionPlans({ subUtils, setIsOpen }) {
   return (
-    <div className="h-full w-full justify-between items-center  bg-white flex flex-col">
+    <div className="h-full w-full justify-between items-center bg-white flex flex-col">
       <h2 className="font-semibold text-black text-3xl">Choose a plan</h2>
 
       <p className="w-[40%] p-2 text-center font-semibold text-white bg-primaryColor/50">
@@ -12,7 +12,7 @@ function SubscriptionPlans({ subUtils, setIsOpen }) {
         Best Subscription plans for Mayraykee
       </p>
 
-      <ul className="flex justify-between gap-3 w-[95%] mt-[3%] h-[70%]">
+      <ul className="flex-1 grid grid-cols-1 md:grid-cols-2 gap-5 w-[95%] mt-[3%] overflow-y-auto">
         {subUtils?.packages?.map((current) => (
           <SubscriptionCard
             setIsOpen={setIsOpen}
