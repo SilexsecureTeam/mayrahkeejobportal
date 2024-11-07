e9b3c91ace2705733e35dd3d70899ab9205e7b64
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-07 13:25:42 +0100

    Update PublicProfile.jsx

--- src/applicant-module/pages/profile/PublicProfile.jsx
+++ src/applicant-module/pages/profile/PublicProfile.jsx
@@ -42,7 +42,7 @@ function PublicProfile() {
                 <div className="mt-[-70px]">
                   <img
                     src={candidate?.profile ? `${IMAGE_URL}/${candidate.profile}` : 'https://via.placeholder.com/150'}
-                    className="w-24 h-24 md:w-32 md:h-32 ring-4 ring-white rounded-full object-cover"
+                    className="max-w-24 max-h-24 md:w-32 md:h-32 ring-4 ring-white rounded-full object-cover"
                     alt="profile"
                   />
                 </div>
