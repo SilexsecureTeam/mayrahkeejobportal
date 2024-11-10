77b70bbb47d2b04de0a44d4905aae123dd30c879
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-10 15:56:40 +0100

    Update RegistrationProgress.jsx

--- src/components/Auth/RegistrationProgress.jsx
+++ src/components/Auth/RegistrationProgress.jsx
@@ -2,27 +2,37 @@
 import { registration_steps_keys } from "../../utils/constants";
 import { registrationType } from "../../reducers/RegistrationReducer";
 import useStaffRoute from "../../routes/useStaffRoute";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function RegistrationProgress({state, dispatch, role}) {
-  const [last, setLast]=useState("");
+function RegistrationProgress({ state, dispatch, role }) {
+  const [last, setLast] = useState("");
+  const keys = Object.keys(registrationType(role));
+  const lastKey = keys[keys.length - 1];
+  const lastStep = registrationType(role)[lastKey];
+
+  useEffect(() => {
+    setLast(lastStep?.title);
+  }, [role]);
+
   const getSteps = () => {
-    return Object.keys(registrationType(role)).map((key, index) => {
+    return keys.map((key, index) => {
       const currentStep = registrationType(role)[key];
-      if([Object.keys(registrationType(role)).length-1][0] ===key){ setLast(currentStep?.title)}
-
-      const isSelected =  (currentStep?.title === state?.title) ? true : false
-      const icon = isSelected ? currentStep?.activeIcon : currentStep?.inactiveIcon
+      const isSelected = currentStep?.title === state?.title;
+      const icon = isSelected ? currentStep?.activeIcon : currentStep?.inactiveIcon;
+      const isLast = index === keys.length - 1;
 
       return (
         <li key={currentStep?.title} className={`w-full flex `}>
           <div className="flex flex-col items-center w-[25%]">
-            <div className={`bg-white rounded-md p-[15px] ${isSelected && 'border-2 border-gray-800 transition duration-1000'}`}>
-              <img className="h-[25px] w-[25px]  " src={icon} />
+            <div
+              className={`bg-white rounded-md p-[15px] ${
+                isSelected && "border-2 border-gray-800 transition duration-1000"
+              }`}
+            >
+              <img className="h-[25px] w-[25px]" src={icon} />
             </div>
-            {currentStep?.title !== last && <hr className="h-[25px] border-dashed border w-0" />}
+            {!isLast && <hr className="h-[25px] border-dashed border w-0" />}
           </div>
-
           <div>
             <p className="font-semibold text-[15px] text-white">{currentStep?.title}</p>
             <p className="font-meduim text-little text-gray-400">{currentStep?.desc}</p>
@@ -31,7 +41,6 @@ function RegistrationProgress({state, dispatch, role}) {
       );
     });
   };
-
 
   return (
     <div className="w-full h-full bg-primaryColor flex items-center justify-center">
