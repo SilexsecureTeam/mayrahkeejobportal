b84c8c92237dec8fe56f37a016e0c3cc1c65b8cc
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-12 11:40:41 +0100

    Update SideCard.jsx

--- src/components/Auth/SideCard.jsx
+++ src/components/Auth/SideCard.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import mayrahkeeIcon from "../../assets/pngs/mayrakee-icon.png";
 import interviewIcon from "../../assets/svgs/interview-icon.svg";
 import managerAttireIcon from "../../assets/manager-business-attire.jpg";
-import mainLogoTwo from "../../assets/happy-couple-of-african-american-business-partners-2023-11-27-05-18-22-utc.jpg";
+import mainSideImg from "../../assets/happy-couple-of-african-american-business-partners-2023-11-27-05-18-22-utc.jpg";
+import mainLogoTwo from "../../assets/pngs/mayrahkee-logo-2.png";
+
 
 function SideCard() {
   return (
     <div className="h-full w-[50%] hidden md:flex flex-col justify-start items-center  bg-primaryColor pt-[5%]">
       <img src={mainLogoTwo} className="w-[50%] h-[15%] object-contain" />
       <img
-        src={"/main-side-icon.jpg"}
+        src={mainSideImg}
         className="h-[50%] mt-[2%] object-cover rounded-[30px] border shadow shadow-gray-800"
       />
       <span className="text-lg mt-5 text-white font-bold">
