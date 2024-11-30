552580749d5a4545e0c67b6f6a60a0cbc8262be2
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-30 04:35:31 -1100

    Update AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,6 +3,7 @@ import Banner from '../components/Landing/Banner';
 import Footer from '../components/Landing/Footer';
 import Navbar from '../components/Landing/Navbar';
 import Empowering from '../components/Landing/Empowering';
+import MissionSection from '../components/Landing/MissionSection';
 const AboutUs=()=>{
     return(
     <div className='relative max-w-[1200px] w-full mx-auto'>
@@ -10,6 +11,7 @@ const AboutUs=()=>{
       <main className="relative my-20 px-5 h-auto">
         <Banner title="About Us" desc="Connecting talent to opportunities with integrity and innovation." />
             <Empowering />
+<MissionSection />
 <Footer /> 
       </main>
     </div>    
