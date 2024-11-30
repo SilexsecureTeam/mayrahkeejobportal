af565a9256c5ed3f749bdbee9d421d882f4a3915
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-30 04:47:15 -1100

    Update AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,7 +3,7 @@ import Banner from '../components/Landing/Banner';
 import Footer from '../components/Landing/Footer';
 import Navbar from '../components/Landing/Navbar';
 import Empowering from '../components/Landing/Empowering';
-import MissionSection from '../components/Landing/MissionSection';
+import InfoSection from '../components/Landing/InfoSection';
 const AboutUs=()=>{
     return(
     <div className='relative max-w-[1200px] w-full mx-auto'>
@@ -11,7 +11,20 @@ const AboutUs=()=>{
       <main className="relative my-20 px-5 h-auto">
         <Banner title="About Us" desc="Connecting talent to opportunities with integrity and innovation." />
             <Empowering />
-<MissionSection />
+<InfoSection
+        title="Our Mission"
+        description="Our mission is to become Africa’s most trusted and comprehensive online platform for recruitment. We strive to provide innovative and customized solutions that seamlessly connect employers, service providers, and job seekers."
+        buttonText="Get Started Now"
+        imageSrc="handshake.jpg"
+        reverse={false}
+      />
+      <InfoSection
+        title="Vision Statement"
+        description="Our vision is to be recognized as Africa’s foremost recruitment partner and employer of choice. We aim to set new benchmarks for innovation, ethical practices, and leadership in the industry."
+        buttonText="Get Started Now"
+        imageSrc="vision.jpg"
+        reverse={true}
+      />
 <Footer /> 
       </main>
     </div>    
