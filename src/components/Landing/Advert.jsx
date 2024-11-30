1e05cdc90e974ea4953985554850478a5a10aca8
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-29 23:31:24 -1100

    Update Advert.jsx

--- src/components/Landing/Advert.jsx
+++ src/components/Landing/Advert.jsx
@@ -8,7 +8,7 @@ const Advert=()=>{
             {/* Left */}
             <section className="flex flex-col gap-3 w-96 my-4"
       style={{
-        backgroundImage: `linear-gradient(rgba(0,100,0,.5), rgba(0,0,100,.5)), url(${tunnel})`,
+        backgroundImage: `linear-gradient(rgba(0,100,0,.5), rgba(0,100,0,.5)), url(${tunnel})`,
         backgroundPosition: 'center',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat'
