3124c07ef152f64828bcff05b31b06f762101115
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-29 23:58:12 -1100

    Update Advert.jsx

--- src/components/Landing/Advert.jsx
+++ src/components/Landing/Advert.jsx
@@ -5,7 +5,7 @@ import approved from '../../assets/pngs/approved.png'
 import {BiBriefcase} from 'react-icons/bi'
 const Advert=()=>{
     return(
-        <div className="w-full min-h-72 my-8 rounded-xl p-8 bg-green-700 text-white flex flex-wrap gap-16 md:gap-10 justify-between items-center" style={{
+        <div className="w-full min-h-72 my-8 rounded-xl p-8 text-white flex flex-wrap gap-16 md:gap-10 justify-between items-center" style={{
         backgroundImage: `linear-gradient(rgba(0,100,0,.5), rgba(0,100,0,.5)), url(${tunnel})`,
         backgroundPosition: 'center',
         backgroundSize: 'cover',
