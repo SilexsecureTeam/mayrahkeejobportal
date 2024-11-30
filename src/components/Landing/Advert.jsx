0400fc3cc1fbf7c2583a531805f050b1569ba738
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-11-30 00:44:33 -1100

    Update Advert.jsx

--- src/components/Landing/Advert.jsx
+++ src/components/Landing/Advert.jsx
@@ -6,6 +6,7 @@ import bgImg from "../../assets/pngs/happy-couple-of-african-american-business-p
 import {BiBriefcase} from 'react-icons/bi'
 const Advert=()=>{
     return(
+<div className="bg-green-700">
         <div className="w-full min-h-72 my-8 rounded-xl p-8 text-white flex flex-wrap gap-16 md:gap-10 justify-between items-center"
        style={{
         backgroundImage: `linear-gradient(rgba(0,100,0,.7), rgba(0,100,0,.7)), url(${tunnel})`,
@@ -43,6 +44,7 @@ const Advert=()=>{
         </div>
             </section>
         </div>
+</div>
     )
 }
 
