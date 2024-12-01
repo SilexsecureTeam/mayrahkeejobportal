094cca95a2e56d6532f50faa931eac18342a7cca
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-12-01 02:07:52 -1100

    Update AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,13 +3,21 @@ import Footer from '../components/Landing/Footer';
 import Navbar from '../components/Landing/Navbar';
 import Empowering from '../components/Landing/Empowering';
 import InfoSection from '../components/Landing/InfoSection';
+import tunnel from '../assets/pngs/Tunnel2.png'
 import missionImg from '../assets/pngs/about3.png'
 import visionImg from '../assets/pngs/about4.png'
 const AboutUs = () => {
     window.scrollTo(0, 0)
     return (
         <>
-            <div className='relative max-w-[1200px] w-full mx-auto'>
+            <div className='relative max-w-[1200px] w-full mx-auto'
+       style={{
+        backgroundImage: `linear-gradient(rgba(150,150,150,.5), rgba(150,150,150,.5)), url(${tunnel})`,
+        backgroundPosition: 'center',
+        backgroundSize: '100% 100%',
+        backgroundRepeat: 'no-repeat',
+        backgroundBlendMode: "overlay"
+      }}>
                 <Navbar />
                 <main className="relative my-20 px-5 h-auto flex flex-col gap-5">
                     <Banner title="About Us" desc="Connecting talent to opportunities with integrity and innovation." />
