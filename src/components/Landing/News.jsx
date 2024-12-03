772b69b4e48ae444694a49e6968c83ae3f00b689
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-12-03 16:57:38 +0100

    Update News.jsx

--- src/components/Landing/News.jsx
+++ src/components/Landing/News.jsx
@@ -21,7 +21,7 @@ const News = () => {
                 {/* Recent view */}
                 {recent && (
                     <section
-                        onClick={()=>{navigate(`/news/${recent?.id); scrollTo(0,0)}`}}
+                        onClick={()=>{navigate(`/news/${recent?.id}`); scrollTo(0,0)}}
                         key={recent?.id}
                         className="w-full h-max md:w-1/2 flex-1 flex flex-col p-4"
                     >
@@ -57,7 +57,7 @@ const News = () => {
                     {news &&
                         news.map((newsItem) => (
                             <section
-                                onClick={()=>{navigate(`/news/${newsItem?.id); scrollTo(0,0)}`}}
+                                onClick={()=>{navigate(`/news/${newsItem?.id}`); scrollTo(0,0)}}
                                 key={newsItem?.id}
                                 className="flex justify-between items-stretch"
                             >
