b4974d3187c6723a3409e736a0a673c6574a366c
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-12-03 17:18:27 +0100

    Update LocationSection.jsx

--- src/components/Landing/LocationSection.jsx
+++ src/components/Landing/LocationSection.jsx
@@ -13,7 +13,7 @@ const LocationSection = () => {
             <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-green-600"></div>
           </div>
         )}
-        <div className="relative w-full h-64 lg:h-[400px] rounded-lg shadow-lg overflow-hidden">
+        <div className="relative w-full h-64 md:h-[400px] rounded-lg shadow-lg overflow-hidden">
           <iframe
             title="Google Map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.939146802545!2d7.487049974913218!3d9.055269691671814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0bc4e816b571%3A0x2bd9bd048ba63e73!2sNICON%20Plaza!5e0!3m2!1sen!2sng!4v1696351330005!5m2!1sen!2sng"
@@ -23,7 +23,7 @@ const LocationSection = () => {
             loading="lazy"
           ></iframe>
           {/* Custom Popup (White Box) */}
-          <div className="absolute bg-white shadow-lg rounded-lg p-3 md:p-4 left-4 bottom-8 lg:bottom-16 w-48 md:w-64">
+          <div className="absolute bg-white shadow-lg rounded-lg p-3 md:p-4 left-4 bottom-8 lg:bottom-16 w-48">
             <h4 className="font-bold text-gray-800 text-sm md:text-base">NICON Plaza</h4>
             <p className="text-xs md:text-sm text-gray-600 mt-1">
               6th Floor, 242 Muhammadu Buhari Way, <br />
@@ -42,7 +42,7 @@ const LocationSection = () => {
       </div>
 
       {/* Text Section */}
-      <div className="w-full lg:w-1/2 mt-6 lg:mt-0 lg:pl-10 text-left">
+      <div className="w-full md:w-1/2 mt-6 lg:mt-0 md:pl-10 text-left">
         <h3 className="text-green-600 font-semibold text-lg uppercase">Our Location</h3>
         <h2 className="text-2xl lg:text-3xl font-bold text-gray-800 mt-2">
           Connecting Near and Far
