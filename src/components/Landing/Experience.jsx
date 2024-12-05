1263daaff43518b3678ad8a08b748646df78390b
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-12-05 11:45:21 -1100

    Update Experience.jsx

--- src/components/Landing/Experience.jsx
+++ src/components/Landing/Experience.jsx
@@ -8,21 +8,22 @@ const Experience=()=>{
                 <h2 className="text-4xl md:max-w-18 font-semibold">Experience With Number</h2>
 
             </section>
-            {/* Right */}
-            <section className="w-full md:w-1/2 flex gap-3 justify-between my-2 md:my-0 divide-x-2 divide-x-gray-500">
-                <div className="flex flex-col gap-3 w-1/3 px-3">
-                    <p className="text-3xl font-semibold text-green-600">92%</p>
-                    <p className="text-xs text-gray-600">Many users find relevant jobs according to skills</p>
-                </div>
-                <div className="flex flex-col gap-3 w-1/3">
-                    <p className="text-3xl font-semibold text-green-600">97%</p>
-                    <p className="text-xs text-gray-600">Data filtering from companies doesn't take long</p>
-                </div>
-                <div className="flex flex-col gap-3 w-1/3">
-                    <p className="text-3xl font-semibold text-green-600">76%</p>
-                    <p className="text-xs text-gray-600">Many top companies can connect with you. </p>
-                </div>
-            </section>
+           {/* Right */}
+<section className="w-full md:w-1/2 flex gap-6 justify-between my-2 md:my-0 divide-x-2 divide-x-gray-500 px-4">
+    <div className="flex flex-col gap-3">
+        <p className="text-3xl font-semibold text-green-600">92%</p>
+        <p className="text-xs text-gray-600">Many users find relevant jobs according to skills</p>
+    </div>
+    <div className="flex flex-col gap-3 px-4">
+        <p className="text-3xl font-semibold text-green-600">97%</p>
+        <p className="text-xs text-gray-600">Data filtering from companies doesn't take long</p>
+    </div>
+    <div className="flex flex-col gap-3 px-4">
+        <p className="text-3xl font-semibold text-green-600">76%</p>
+        <p className="text-xs text-gray-600">Many top companies can connect with you.</p>
+    </div>
+</section>
+
             
         </div>
     )
