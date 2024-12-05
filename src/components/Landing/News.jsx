6ae81dff8d6acc32a9d2a7405023816762824847
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-12-05 01:16:06 -1100

    Update News.jsx

--- src/components/Landing/News.jsx
+++ src/components/Landing/News.jsx
@@ -16,6 +16,13 @@ const News = () => {
             isDataNeeded: true,
         }));
     }, []);
+const calculateReadingTime = (text) => {
+    const wordsPerMinute = 200;
+    const words = text.split(/\s+/).length;
+    const minutes = Math.ceil(words / wordsPerMinute);
+    return minutes;
+};
+
 
     useEffect(() => {
     if (getAllBlogPosts?.data) {
