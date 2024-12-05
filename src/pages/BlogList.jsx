d5fcd402159453aba9bd341502b5e7ffa196c08b
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2024-12-05 01:32:44 -1100

    Update BlogList.jsx

--- src/pages/BlogList.jsx
+++ src/pages/BlogList.jsx
@@ -185,7 +185,7 @@ const calculateReadingTime = (text) => {
 
                     {/* Blog Posts */}
                     {isLoading ? (
-                        <div className="flex flex-row-reverse justify-center items-center mt-10">
+                        <div className="flex justify-center items-center mt-10">
                             <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-green-600"></div>
                         </div>
                     ) : filteredBlogs?.length > 0 ? (
