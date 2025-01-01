ee4a00bb1e417f2378ca908bd88da6d22d774617
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-01-01 21:36:52 +0100

    Update SideBarItem.jsx

--- src/company-module/components/SideBarItem.jsx
+++ src/company-module/components/SideBarItem.jsx
@@ -39,5 +39,48 @@ const {setAuthDetails} = useContext(AuthContext)
     </li>
   );
 }
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContex";
+
+function SideBarItem({ data, dispatch, state, setIsOpen }) {
+  const navigate = useNavigate();
+const {setAuthDetails} = useContext(AuthContext)
+ 
+  const navigateToPage = () => {
+    if (data.type === "LOG-OUT") {
+      localStorage.clear();
+      setAuthDetails(null);
+      navigate(data.route, { replace: true });
+    } else {
+      dispatch({ ...data });
+      navigate(data.route);
+    }
+    setIsOpen(false)
+  };
+
+  return (
+    <li
+      onClick={navigateToPage}
+      className={`cursor-pointer flex gap-[10px] hover:bg-green-800 group items-center z-10 p-[5px] ${
+        state?.type === data?.type ? "bg-primaryColor" : "bg-none"
+      }`}
+    >
+      <img
+        className="h-[20px] w-[20px]"
+        src={state?.type === data?.type ? data.iconActive : data.icon}
+      />
+      <span
+        className={`${
+          state?.type === data?.type ? "text-white" : "text-primary"
+        } text-sm group-hover:text-white`}
+      >
+        {data.title}
+      </span>
+    </li>
+  );
+}
+
+export default React.memo(SideBarItem);
 
 export default React.memo(SideBarItem);
