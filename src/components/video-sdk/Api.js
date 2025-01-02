d3d7ab2cb3bfb1c50820c47aa2c07ff88aeb7ebd
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-01-02 18:14:31 +0100

    Update Api.js

--- src/components/video-sdk/Api.js
+++ src/components/video-sdk/Api.js
@@ -1,8 +1,7 @@
 import { onFailure } from "../../utils/notifications/OnFailure";
 
 let authToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiJhOTE1MzJhYi1hOGM1LTRjYjEtYTY4Ni1mNWIyY2UyNzQwZmIiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTczNTY3NjY2OSwiZXhwIjoxNzM2MjgxNDY5fQ.terwR0Tx8tiZgNvGvy5oi1UDIL-1dFnvhahON4-JUYc";
-
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI1Nzk3NWJmMi0zNGZkLTQzMGUtOTU4Mi1lMjBiODI3ZWQ5NTIiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTczNTgzNzI3NCwiZXhwIjoxNzM2NDQyMDc0fQ.oVghHaR0B0Pog6jq_v_r1Ibq0AyEVCFxmLvqTUqgdzQ";
 // Getter for the current auth token
 export const getAuthToken = () => authToken;
 
