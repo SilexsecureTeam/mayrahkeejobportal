1c0b96c04bcf345b90a38f6d4613f57762981ab8
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-01-16 21:43:24 +0100

    Update RoundCharts.jsx

--- src/applicant-module/components/charts/RoundCharts.jsx
+++ src/applicant-module/components/charts/RoundCharts.jsx
@@ -17,7 +17,7 @@ const RoundChart = ({ data }) => {
         width: "100%",
       },
       labels: Object.keys(applicationStatuses || {}), // Status labels
-      colors: ["#FF9900", "#00E396", "#008FFB", "#8B0A1A"], // Colors for each section
+      colors: ["#FF9900", "#FFFF00", "#00E396", "#008FFB", "#8B0A1A"], // Colors for each section
       legend: {
         position: "bottom",
         labels: {
