d29d608e5bc71dcfa4929915257becdc19a8681d
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-01-27 23:10:53 +0100

    Update DescriptionItem.jsx

--- src/company-module/components/job-posting/DescriptionItem.jsx
+++ src/company-module/components/job-posting/DescriptionItem.jsx
@@ -8,7 +8,7 @@ function DescriptionItem({ data, jobUtils }) {
   return (
     <div className="flex flex-col md:flex-row border-b py-2 min-h-[170px]">
       <div className="flex flex-col w-full md:max-w-[25%] gap-[10px]">
-        <h3 className="text-gray-700 text-sm font-semibold">{data.title}</h3>
+        <h3 className="text-gray-700 text-sm font-semibold flex gap-1">{data.title} {data?.required && <strong className="text-red-500">*</strong>}</h3>
         <span className="text-little text-gray-600">{data.desc}</span>
       </div>
 
