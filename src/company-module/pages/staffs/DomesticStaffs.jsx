0f6ac6460eae47761ef23a5f1575f12a88e499de
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-01-28 01:25:25 +0100

    Update DomesticStaffs.jsx

--- src/company-module/pages/staffs/DomesticStaffs.jsx
+++ src/company-module/pages/staffs/DomesticStaffs.jsx
@@ -128,7 +128,7 @@ function DomesticStaff() {
             onClick={() => setConditions(!conditions)}
           />
           <h1 className="text-xl font-bold">Job Descriptions</h1>
-          <p className="text-sm overflow-y-auto flex-1">
+          <p className="flex-1 text-sm overflow-y-auto">
            
             {selectedCategory?.description}
           </p>
