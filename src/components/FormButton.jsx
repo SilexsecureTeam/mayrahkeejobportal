0772fe5bc58a4c634ad2fee94fed10284ba1aca0
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-01-28 01:30:06 +0100

    Update FormButton.jsx

--- src/components/FormButton.jsx
+++ src/components/FormButton.jsx
@@ -5,7 +5,7 @@ function FormButton({ children, loading, condition=false, onClick, height = 'h-[
   return onClick ? (
     <button
       onClick={onClick}
-      className={`overflow-hidden text-little ${
+      className={`flex-shrink-0 overflow-hidden text-little ${
         !loading
           ? "hover:text-[13px] hover:scale-105 duration-75"
           : "hover:text-little hover:scale-100 duration-75"
@@ -18,7 +18,7 @@ function FormButton({ children, loading, condition=false, onClick, height = 'h-[
   ) : (
     <button
       type="submit"
-      className={`overflow-hidden text-small ${
+      className={`flex-shrink-0 overflow-hidden text-small ${
         !loading
           ? "hover:text-[13px] hover:scale-105 duration-75"
           : "hover:text-little hover:scale-100 duration-75"
