739e91c9b21d88e5211f74ba865cfeb0aec24290
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-01-29 21:19:45 +0100

    Update SubscriptionPackages.jsx

--- src/components/subscription/SubscriptionPackages.jsx
+++ src/components/subscription/SubscriptionPackages.jsx
@@ -12,7 +12,7 @@ function SubscriptionPackages() {
   return (
     (
       <div className="h-full z-50 w-full text-gray-400 text-little flex justify-center items-center">
-        <div className="w-full h-[95%] p-4 flex flex-col rounded-[10px]  bg-white border">
+        <div className="w-full min-h-full p-4 flex flex-col rounded-[10px] bg-white border">
           <SubscriptionPlans
             packages={subUtils.packages}
             
