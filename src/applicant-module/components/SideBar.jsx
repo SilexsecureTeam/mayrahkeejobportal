f76b0743c0cf7256a3e0224cec325a2ea33f3bb6
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-02-09 13:31:27 +0100

    Update SideBar.jsx

--- src/applicant-module/components/SideBar.jsx
+++ src/applicant-module/components/SideBar.jsx
@@ -127,7 +127,7 @@ function SideBar({ children, authDetails, toogleIsOpen, isMenuOpen }) {
         {/* User Info */}
         <div className="absolute bottom-0 right-3 p-2 flex gap-3 items-end w-[300px]">
           <div className="flex flex-col truncate">
-            <span className="text-secondaryColor text-sm font-semibold truncate
+            <span className="text-secondaryColor text-sm font-semibold truncate">
               {candidate?.full_name ? candidate?.full_name : `${authDetails?.user?.first_name || "N/A"} ${
                 authDetails?.user?.last_name || "N/A"
               }`}
