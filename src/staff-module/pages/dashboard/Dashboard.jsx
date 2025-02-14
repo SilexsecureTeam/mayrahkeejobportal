35525d9392f477ce2e46d72ecf3d99c9848e708a
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-02-14 12:22:31 +0100

    Update Dashboard.jsx

--- src/staff-module/pages/dashboard/Dashboard.jsx
+++ src/staff-module/pages/dashboard/Dashboard.jsx
@@ -19,8 +19,7 @@ import DefaultSwitch from "../../../components/DefaultSwitch";
 import useStaffUser from "../../../hooks/useStaffUser";
 import { onFailure } from "../../../utils/notifications/OnFailure";
 import { axiosClient } from "../../../services/axios-client";
-import { GoDiscussionOutdated } from "react-icons/go";
-import { GrUpload } from "react-icons/gr";
+import { GrDocumentText, GrUpload } from "react-icons/gr";
 function Dashboard() {
   const { authDetails } = useContext(AuthContext);
   const { setSideBar } = useContext(StaffRouteContext);
