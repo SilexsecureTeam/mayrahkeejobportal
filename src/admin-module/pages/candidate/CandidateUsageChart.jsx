150733d7a8f3ee1308347ff028e8079b96b5c5ae
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-03-06 10:05:17 +0100

    Update CandidateUsageChart.jsx

--- src/admin-module/pages/candidate/CandidateUsageChart.jsx
+++ src/admin-module/pages/candidate/CandidateUsageChart.jsx
@@ -29,7 +29,7 @@ export default function CandidateUsageChart({ candidates }) {
     {
       label: 'Pending',
       value: candidates.filter(c => c.status === 'pending').length,
-      color: '#F7DC6F',
+      color: 'yellow',
     },
   ];
 
