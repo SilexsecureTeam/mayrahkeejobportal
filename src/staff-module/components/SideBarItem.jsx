061df737b250b795213488062963da95fd100aba
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-03-22 06:41:08 +0100

    Update SideBarItem.jsx

--- src/staff-module/components/SideBarItem.jsx
+++ src/staff-module/components/SideBarItem.jsx
@@ -1,13 +1,16 @@
 import { clear } from "idb-keyval";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContex";
 
 function SideBarItem({ data, dispatch, state, setIsOpen }) {
   const navigate = useNavigate();
-
+const {setAuthDetails} = useContext(AuthContext)
   const navigateToPage = () => {
     if (data.type === "LOG-OUT") {
       sessionStorage.clear()
+      dispatch({});
+      setAuthDetails(null)
       navigate(data.route, { replace: true });
     } else {
       dispatch({ ...data });
