85fc7ee172a6d9ad8ea87d9a179f097c8d4772af
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-04-25 16:14:05 +0100

    Update base.js

--- src/utils/base.js
+++ src/utils/base.js
@@ -1,3 +1,5 @@
 export const BASE_URL =import.meta.env.VITE_BASE_URL;
 export const IMAGE_URL = import.meta.env.VITE_IMAGE_URL;
 export const CONMPANY_TAX= 200;
+export const DOMESTIC="domestic";
+export const ARTISAN="artisan";
