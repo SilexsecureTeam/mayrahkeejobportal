6f04557580d0446deeb9588bb1054b3d615c8d41
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-05-08 03:00:30 +0100

    Update StatsCard.jsx

--- src/company-module/components/home/StatsCard.jsx
+++ src/company-module/components/home/StatsCard.jsx
@@ -4,7 +4,7 @@ function StatsCard({ data, value, onClick }) {
   return (
     <li
       onClick={onClick}
-      className={`md:w-[25%] w-[32%] hover:scale-105 cursor-pointer duration-100 text-white h-full flex items-center justify-between p-2 ${data.bg_color}`}
+      className={`md:w-[25%] w-[32%] hover:scale-105 cursor-pointer duration-100 text-white min-h-12 flex items-center justify-between p-2 ${data.bg_color}`}
     >
       <h2 className="md:text-xl text-lg font-bold">{value.length}</h2>
       <span className="md:text-sm text-little text-center">{data.title}</span>
