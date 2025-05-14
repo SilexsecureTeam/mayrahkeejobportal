34c5c1a2ccde2bfc292734519c07dbf55b11be4f
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-05-14 14:28:11 +0100

    Update JobGridCard.jsx

--- src/applicant-module/pages/jobs/components/JobGridCard.jsx
+++ src/applicant-module/pages/jobs/components/JobGridCard.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { resourceUrl } from '../../../../services/axios-client';
 
-const JobGridCard = ({ newApplicant, job }) => {
+const JobGridCard = ({ newApplicant, job,getAllApplications }) => {
     const navigate = useNavigate();
     const keywordArr = job.search_keywords?.split(',');
-
+const hasApplied = getAllApplications?.some((app) => app.job_id === job.id);
+    
     return (
         <div
             onClick={() => navigate("/applicant/find-job/id", { state: { job: job } })}
@@ -31,10 +32,22 @@ const JobGridCard = ({ newApplicant, job }) => {
                 <div>
                     {/* <div className="flex my-4 bg-gray-100 h-2 rounded">
                         <div className="bg-[#56CDAD] w-[50%] h-full rounded"></div>
-                    </div> */}
+                    </div> 
                     <p className="text-sm">
                         <b>{job?.number_of_participants} Expected Applicants</b> 
-                    </p>
+                    </p>*/}
+                    <div className="mt-4 md:mt-0 flex items-center">
+                    <button
+                        disabled={hasApplied}
+                        className={`w-full md:w-[150px] p-2 text-white rounded-md transition-colors ${
+                            hasApplied
+                                ? 'bg-gray-400 cursor-not-allowed'
+                                : 'bg-green-700 hover:bg-green-900'
+                        }`}
+                    >
+                        {hasApplied ? 'Applied' : 'Apply'}
+                    </button>
+                </div>
                 </div>
             </div>
         </div>
