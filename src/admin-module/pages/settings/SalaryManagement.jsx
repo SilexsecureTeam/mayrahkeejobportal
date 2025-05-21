578a67949fc952d799c2c5838a09dab832c5f225
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-05-21 20:03:25 +0100

    Update SalaryManagement.jsx

--- src/admin-module/pages/settings/SalaryManagement.jsx
+++ src/admin-module/pages/settings/SalaryManagement.jsx
@@ -25,7 +25,7 @@ const SalaryManagement = () => {
   const handleUpdate = async (updatedStaff) => {
     try {
       await axios.put(`${import.meta.env.VITE_BASE_URL}/salary/update`, {
-        domestic_staff_id: updatedStaff?.id
+        domestic_staff_id: updatedStaff?.id,
         salary_agreed: updatedStaff?.salary_agreed,
         service_charge: updatedStaff?.service_charge,
       });
