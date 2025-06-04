4a8c0505cfb309f396d33be579c8fac1a670440d
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-06-04 08:26:37 +0100

    Update Home.jsx

--- src/applicant-module/pages/home/Home.jsx
+++ src/applicant-module/pages/home/Home.jsx
@@ -186,12 +186,12 @@ const interviews = allApplications?.filter(
               <div className="pt-1 h-full lg:w-full w-[45%]  lg:h-1/2">
                 <div
                   onClick={()=>navigateToApplications("interview")}
-                  className="border bg-yellow-300 text-gray-600 transition duration-400 h-full cursor-pointer mb-4 p-3 pb-0 flex flex-col justify-between"
+                  className="border bg-yellow-300 text-white transition duration-400 h-full cursor-pointer mb-4 p-3 pb-0 flex flex-col justify-between"
                 >
                   <p className="font-bold capitalize">Interviewed</p>
                   <div className="flex justify-between items-end mt-4">
                     <p className="text-6xl font-medium">{interviews?.length || 0}</p>
-                      <GoDiscussionOutdated size="30" className="mb-2 border border-gray-600/20" />
+                      <GoDiscussionOutdated size="30" className="mb-2 border border-gray-200/20" />
                   </div>
                 </div>
               </div>
